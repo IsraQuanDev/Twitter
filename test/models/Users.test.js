9253cf1a9a314c10e65c9f22de1ac40d7ca16e04
Francisco Israel Mecillas <francisco_polisol@hotmail.com>
2022-05-02 22:50:42 -0500

    3. añadiendo metodos get & set para pruebas de unidad

--- test/models/Users.test.js
+++ test/models/Users.test.js
@@ -4,7 +4,8 @@ describe("Unit Tests for User class", ()=>{
 
     test('Create an User object', ()=>{
         // Aquí invocas el código que vas a usar en tu app
-        const user = new User(1, "carlogilmar","Carlo","Bio","dateCreated","lastUpdated")
+        // const user = new User(1, "carlogilmar","Carlo","Bio","dateCreated","lastUpdated")
+        const user = new User(1, "carlogilmar","Carlo","Bio")
     //Aquí validas los resultados de ese código
     // Esta es una comparación que va a igualar el valor de la izquierda con el valor de la derecha (valor esperado)4
 
@@ -12,8 +13,44 @@ describe("Unit Tests for User class", ()=>{
     expect(user.username).toBe("carlogilmar")
     expect(user.name).toBe("Carlo")
     expect(user.bio).toBe("Bio")
-    expect(user.dateCreated).toBe("dateCreated")
-    expect(user.lastUpdated).toBe("lastUpdated")
+    // expect(user.dateCreated).toBe("dateCreated")
+    // expect(user.lastUpdated).toBe("lastUpdated")
     
+    expect(user.dateCreated).not.toBeUndefined() //verifica que el valor no sea undefined
+    expect(user.lastUpdated).not.toBeUndefined()
     });
+
+    test('Add getters', ()=>{
+        // Aquí invocas el código que vas a usar en tu app
+        // const user = new User(1, "carlogilmar","Carlo","Bio","dateCreated","lastUpdated")
+        const user = new User(1, "carlogilmar","Carlo","Bio")
+    //Aquí validas los resultados de ese código
+    // Esta es una comparación que va a igualar el valor de la izquierda con el valor de la derecha (valor esperado)4
+
+     
+    expect(user.getUsername).toBe("carlogilmar")
+    expect(user.getBio).toBe("Bio")
+    expect(user.getDateCreated).not.toBeUndefined() //verifica que el valor no sea undefined
+    expect(user.getLastUpdated).not.toBeUndefined()
+    });
+
+
+    test('Add setters', ()=>{
+        // Aquí invocas el código que vas a usar en tu app
+        // const user = new User(1, "carlogilmar","Carlo","Bio","dateCreated","lastUpdated")
+        const user = new User(1, "carlogilmar","Carlo","Bio")
+    //Aquí validas los resultados de ese código
+    // Esta es una comparación que va a igualar el valor de la izquierda con el valor de la derecha (valor esperado)4
+
+    user.setUsername ="Gilmar"
+    expect(user.username).toBe("Gilmar")
+
+    user.setBio = "New bio"
+    expect(user.bio).toBe("New bio")
+
+    });
+
+
+
+
 })
